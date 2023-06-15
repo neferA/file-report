50cb8beb3c031d6ac02a7b7e6a59f30a99700885
Kevin <326.osito@gmail.com>
2023-06-15 11:20:27 -0400

    Add card on Accounts view to see information

--- resources/views/usuarios/account.blade.php
+++ resources/views/usuarios/account.blade.php
@@ -27,8 +27,6 @@
                                                 <p class="text-muted mb-1">{{ $user-> email }}</p>
                                                 @if ($role)
                                                 <p class="text-muted mb-4">{{ $role->name }}</p>
-                                                @else
-                                                <p>No tiene un rol asignado. Por favor, contacte al administrador.</p>
                                                 @endif
                                                 <div class="d-flex justify-content-center mb-2">
                                                     <!-- <button type="button" class="btn btn-primary">Follow</button> -->
@@ -37,58 +35,46 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <!-- <div class="col-lg-8"> -->
-                                    <!--     <div class="card mb-4"> -->
-                                    <!--         <div class="card-body"> -->
-                                    <!--             <div class="row"> -->
-                                    <!--                 <div class="col-sm-3"> -->
-                                    <!--                     <p class="mb-0">Full Name</p> -->
-                                    <!--                 </div> -->
-                                    <!--                 <div class="col-sm-9"> -->
-                                    <!--                     <p class="text-muted mb-0">Johnatan Smith</p> -->
-                                    <!--                 </div> -->
-                                    <!--             </div> -->
-                                    <!--             <hr> -->
-                                    <!--             <div class="row"> -->
-                                    <!--                 <div class="col-sm-3"> -->
-                                    <!--                     <p class="mb-0">Email</p> -->
-                                    <!--                 </div> -->
-                                    <!--                 <div class="col-sm-9"> -->
-                                    <!--                     <p class="text-muted mb-0">example@example.com</p> -->
-                                    <!--                 </div> -->
-                                    <!--             </div> -->
-                                    <!--             <hr> -->
-                                    <!--             <div class="row"> -->
-                                    <!--                 <div class="col-sm-3"> -->
-                                    <!--                     <p class="mb-0">Phone</p> -->
-                                    <!--                 </div> -->
-                                    <!--                 <div class="col-sm-9"> -->
-                                    <!--                     <p class="text-muted mb-0">(097) 234-5678</p> -->
-                                    <!--                 </div> -->
-                                    <!--             </div> -->
-                                    <!--             <hr> -->
-                                    <!--             <div class="row"> -->
-                                    <!--                 <div class="col-sm-3"> -->
-                                    <!--                     <p class="mb-0">Mobile</p> -->
-                                    <!--                 </div> -->
-                                    <!--                 <div class="col-sm-9"> -->
-                                    <!--                     <p class="text-muted mb-0">(098) 765-4321</p> -->
-                                    <!--                 </div> -->
-                                    <!--             </div> -->
-                                    <!--             <hr> -->
-                                    <!--             <div class="row"> -->
-                                    <!--                 <div class="col-sm-3"> -->
-                                    <!--                     <p class="mb-0">Address</p> -->
-                                    <!--                 </div> -->
-                                    <!--                 <div class="col-sm-9"> -->
-                                    <!--                     <p class="text-muted mb-0">Bay Area, San Francisco, CA</p> -->
-                                    <!--                 </div> -->
-                                    <!--             </div> -->
-                                    <!--         </div> -->
-                                    <!--     </div> -->
-                                    <!--     <div class="row"> -->
-                                    <!--     </div> -->
-                                    <!-- </div> -->
+                                    <div class="col-lg-8">
+                                        <div class="card mb-4">
+                                            <div class="card-body">
+                                                <div class="row">
+                                                    <div class="col-sm-3">
+                                                        <p class="mb-0">Nombre Completo</p>
+                                                    </div>
+                                                    <div class="col-sm-9">
+                                                        <p class="text-muted mb-0">{{ $user->name }}</p>
+                                                    </div>
+                                                </div>
+                                                <hr>
+                                                <div class="row">
+                                                    <div class="col-sm-3">
+                                                        <p class="mb-0">E-mail</p>
+                                                    </div>
+                                                    <div class="col-sm-9">
+                                                        <p class="text-muted mb-0">{{ $user->email }}</p>
+                                                    </div>
+                                                </div>
+                                                <hr>
+                                                <div class="row">
+                                                @if ($role)
+                                                    <div class="col-sm-3">
+                                                        <p class="mb-0">Rol</p>
+                                                    </div>
+                                                    <div class="col-sm-9">
+                                                        <p class="text-muted mb-0">{{ $role->name }}</p>
+                                                    </div>
+                                                    @else
+                                                    <div class="col-sm-9">
+                                                        <p class="text-muted mb-0">No tiene un rol asignado. Por favor, contacte al administrador.</p>
+                                                    </div>
+                                                </div>
+                                                @endif
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </section>
