65e1512139b545643c93443823ff437d7818b1e7
Kevin <326.osito@gmail.com>
2023-06-16 22:46:47 -0400

    rollback web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RolController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 use Spatie\Permission\Middlewares\PermissionMiddleware;
 
@@ -24,6 +24,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::middleware(['auth', 'verified'])
+    ->name('account')
+    ->group(function () {
+    Route::get('/account', function () {
+        return view('usuarios.cuenta');
+    });
+});
+
 // middleware superadmin access
 Route::middleware(['auth', 'verified', 'can:crud-usuario'])
     ->name('dashboard')
@@ -32,23 +40,35 @@ Route::middleware(['auth', 'verified', 'can:crud-usuario'])
             return view('dashboard');
         });
 });
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     // show account
-    Route::get('/account', [UserController::class, 'account_details'])->name('usuarios.account');
+    Route::get('/account', [UsuarioController::class, 'account_details'])->name('usuarios.account');
 });
 
 Route::group(['middleware' => ['auth']], function() {
     Route::resource('roles', RolController::class);
-    Route::resource('users', UserController::class);
+    Route::resource('usuarios', UsuarioController::class);
     Route::resource('blogs', BlogController::class);
 });
 
-// guest/auth users enter this routes
-Route::get('/cloud', [UserController::class, 'cloud'])->name('cloud');
-Route::get('/home', [UserController::class, 'home'])->name('home');
+// guest users
+Route::middleware('guest')->group(function () {
+    // Access Routes for guest users
+    Route::get('/home', function () {
+        return view('usuarios.home');
+    });
+    Route::get('/cloud', function () {
+        return view('cloud.index');
+    });
+});
+
+// cloud routes guest and with priviligies
 
 require __DIR__.'/auth.php';
